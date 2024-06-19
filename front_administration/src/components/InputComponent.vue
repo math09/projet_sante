2344e78d9640993155a2d8109b2a4f876e3c102e
DRmatteo74 <mdirienzo74@gmail.com>
2024-06-19 23:33:54 +0200

    Init VueJS project and add login view

---
+++ front_administration/src/components/InputComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="flex flex-col">
+      <label v-if="label" :for="id" class="mb-2 font-semibold text-md" :class="labelClass">{{ label }}</label>
+      <input
+        :id="id"
+        :type="type"
+        :value="value"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        v-model="test"
+        class="p-5 text-xl border border-gray-50 focus:outline-none focus:border-gray-200 bg-neutral-100 shadow-xl rounded-2xl"
+        :class="inputClass"
+      />
+    </div>
+</template>
+
+<script setup>
+import { defineModel } from 'vue';
+
+const test = defineModel('modelValue');
+</script>
+
+<script>
+  export default {
+    name: 'InputComponent',
+    props: {
+      id: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'text'
+      },
+      value: {
+        type: String,
+        default: ''
+      },
+      placeholder: {
+        type: String,
+        default: ''
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      label: {
+        type: String,
+        default: ''
+      },
+      inputClass: {
+        type: String,
+        default: ''
+      },
+      labelClass: {
+        type: String,
+        default: ''
+      }
+    }
+  };
+</script>
---
+++ front_administration/src/components/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button
+      :class="buttonClass"
+      class="px-20 py-3 bg-gradient-to-r from-teal-500 to-teal-300 shadow-xl rounded-2xl text-xl font-bold"
+      :disabled="disabled"
+    >
+      <slot></slot>
+    </button>
+</template>
+  
+<script>
+  export default {
+    name: 'PrimaryButton',
+    props: {
+      buttonClass: {
+        type: String,
+        default: ''
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+</script>
