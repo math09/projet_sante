b8c710c638f2a8809e463fd25d8e173f71b325a7
Hernandez Mathis <hernandezmathis@gmail.com>
2024-07-03 21:31:43 +0200

    Mongo faker part 2

---
+++ api_mongo/README.md
@@ -0,0 +1,3 @@
+#### Generate Faker
+
+docker exec projet_sante-app_mongo-1 python generate_all_data.py
--- api_mongo/generate_all_data.py
+++ api_mongo/generate_all_data.py
@@ -9,7 +9,8 @@ fake = Faker()
 
 client = MongoClient(Config.MONGO_URI)
 db = client[Config.MONGO_DBNAME]
-collection = db['patients']
+patient = db['patient']
+admin = db['administration']
 
 def generate_patients(num_records):
     patients = []
@@ -40,9 +41,28 @@ def generate_patients(num_records):
         patients.append(patient)
     return patients
 
-patients_to_insert = generate_patients(num_records=10)
+def generate_administration(num_records):
+    admins = []
+    for _ in range(num_records):
+        patient = {
+            'prenom': fake.first_name(),
+            'nom': fake.last_name(),
+            'id_administratif': fake.random_number(digits=9, fix_len=True),
+            'mdp': 'password',
+            'role': fake.job(),
+            'date_creation': fake.date_time_between(start_date='-2y', end_date='now').isoformat(),
+            'isActif': fake.boolean(),
+            'email': fake.email(),
+            'num_telephone': fake.random_number(digits=11, fix_len=True),
+        }
+        admins.append(patient)
+    return admins
 
-result = collection.insert_many(patients_to_insert)
-print(f"Inserted {len(result.inserted_ids)} patients")
+patients_to_insert = generate_patients(num_records=10)
+admin_to_insert = generate_administration(num_records=10)
+
+result = patient.insert_many(patients_to_insert)
+result = admin.insert_many(admin_to_insert)
+print(f"Inserted {len(result.inserted_ids)} patient and admin")
 
 client.close()
